65b5235e33196b5937738a78d4bb5e47faca3bae
Angela Gilhotra <angelagilhotra@gmail.com>
2024-09-26 03:16:54 -0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname:"127.0.0.1",
+        port: "3001"
+      },
+      {
+        protocol: "https",
+        hostname:"kbx-landing.vercel.app",
+      }
+    ]
+  }
+};
 
 export default nextConfig;
